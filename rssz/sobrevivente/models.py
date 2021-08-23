e0672eed3f54ecc34131a5d3ebdc87dba13819fd
Victor <victor_ps123@live.com>
2021-08-23 20:32:19 -0300

    iniciando o sobrevivente

--- rssz/sobrevivente/models.py
+++ rssz/sobrevivente/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+class Ultimolocal(models.Model):
+
+    latitude = models.CharField(
+        verbose_name="Latitude",
+        max_length=200,
+        blank=False,
+        null=False
+    )
+
+    longitude = models.CharField(
+        verbose_name="Longitude",
+        max_length=200,
+        blank=False,
+        null=False
+    )
+
+    def __str__(self):
+        return self.latitude + " - " + self.longitude
+
+
+class Sobrevivente(models.Model):
+
+    opcoes_de_sexo = (
+        ('Masculino', 'Masculino'),
+        ('Feminino', 'Feminino')
+    )
+
+    nome = models.CharField(
+        verbose_name="Nome do Sobrevivente",
+        max_length=200,
+        blank=False,
+        null=False
+    )
+
+    idade = models.IntegerField(
+        verbose_name="Idade",
+        blank=False,
+        null=False
+    )
+
+    sexo = models.CharField(
+        verbose_name="Sexo",
+        max_length=50,
+        choices=opcoes_de_sexo,
+        blank=False,
+        null=False
+    )
+
+    infectado = models.BooleanField(
+        verbose_name="Infectado?",
+        default=False
+    )
+
+    ultimo_local = models.ForeignKey(
+        Ultimolocal,
+        on_delete=models.CASCADE,
+        related_name='ultimo_local',
+        null=False,
+        blank=False
+    )
+
+    def __str__(self):
+        return self.nome
